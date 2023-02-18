ba5f6580224ab07c6a6e0d5abfebb612f9dc680b
Michael Ziaja <mchlzj@gmail.com>
2023-02-18 21:33:13 +0100

    first commit

---
+++ src/main/java/com/ReadyForTest/Analyzer/App.java
@@ -0,0 +1,13 @@
+package com.ReadyForTest.Analyzer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
